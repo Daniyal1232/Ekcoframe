881ab48b1dd67721187673d493be8c419874a40c
Daniyal1232 <daniyalbhary10@gmail.com>
2026-09-08 22:26:35 +0330

    Update compile SDK to Android 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 android {
     namespace = "com.artivivelite.app"
-    compileSdk = 35
+    compileSdk = 36
     defaultConfig {
         applicationId = "com.artivivelite.app"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 2
         versionName = "0.2.0"
     }
